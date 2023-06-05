9505a9210509f1614947329a4d6df929ac8112f3
kelvinleandro <kelvinleandro5@gmail.com>
2023-06-05 20:04:22 -0300

    add product description on ProductCard

--- src/components/product-card/ProductCard.js
+++ src/components/product-card/ProductCard.js
@@ -3,15 +3,18 @@ import './ProductCard.css'
 import { Link } from 'react-router-dom';
 
 const ProductCard = ({product}) => {
-  const {nome, preco, img} = product;
+  const {nome, preco, img, descricao} = product;
   const pathName = `/catalogo/${nome}`;
 
   return (
     <div className='product-card w-full shadow-xl hover:scale-105 duration-300'>
-      <img src={img} alt={nome} className='w-40 mx-auto' />
-      <h2 className='font-bold text-xl text-center'>{nome}</h2>
-      <p className='font-medium text-lg text-center'>{preco}</p>
-      <Link to={pathName} className='w-[50%] my-btn blue-btn hover:scale-105'>COMPRAR</Link>
+      <img src={img} alt={nome} className='w-40 mx-auto mb-2' />
+      <h2 className='font-bold text-xl text-center mb-2'>{nome}</h2>
+      <p className='text-justify text-sm mb-2'>{descricao}</p>
+      <div className='flex items-center mx-auto'>
+        <h4 className='font-medium text-md text-center mr-2'>{preco}</h4>
+        <Link to={pathName} className='my-btn blue-btn hover:scale-105 my-auto'>COMPRAR</Link>
+      </div>
     </div>
   )
 }
