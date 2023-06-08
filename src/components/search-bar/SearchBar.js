a36e9466bc8817f51e11f97fdff30c7b66e037af
kelvinleandro <kelvinleandro5@gmail.com>
2023-06-08 20:58:16 -0300

    Search Bar results removed

--- src/components/search-bar/SearchBar.js
+++ src/components/search-bar/SearchBar.js
@@ -1,4 +1,4 @@
-import React, {useState ,useEffect} from 'react'
+import React, {useState} from 'react'
 import {FaSearch} from 'react-icons/fa'
 import {AiOutlineClose} from 'react-icons/ai'
 
@@ -6,37 +6,10 @@ import './SearchBar.css'
 
 const SearchBar = () => {
   const [searchInput, setSearchInput] = useState("");
-  const [data, setData] = useState([]);
-  const [filteredProducts, setFilteredProducts] = useState([]);
-
-  const getFilteredProducts = (e) => {
-    const value = e.target.value;
-    setSearchInput(value);
-    const filtered = data.filter(product => {
-      return product.nome.toLowerCase().includes(value.toLowerCase());
-    });
-
-    if (value === "") {
-      setFilteredProducts([]);
-    } else {
-      setFilteredProducts(filtered);
-    }
-  };
-  
-  const fetchData = () => {
-    fetch("https://apitrainees.herokuapp.com/celulares")
-    .then((response) => response.json())
-    .then((data) => setData(data));
-  };
 
   const clearInput = () => {
-    setFilteredProducts([]);
-    setSearchInput([]);
+    setSearchInput("");
   }
-
-  useEffect(() => {
-    fetchData();
-  }, []);
 
   return (
     <div className='search-container'>
@@ -44,30 +17,19 @@ const SearchBar = () => {
         <input
           type="search"
           placeholder="Busque aqui"
-          onChange={getFilteredProducts}
+          onChange={e => setSearchInput(e.target.value)}
           value={searchInput} />
+
         <div className="search-icon">
-          {filteredProducts.length === 0 ? (
+          {searchInput.length === 0 ? (
             <FaSearch size={20} />
           ) : (
             <AiOutlineClose size={20} id="clear-btn" onClick={clearInput} />
           )}
         </div>
+
       </div>
         
-      {/* {filteredProducts.length > 0 && (
-        <div className="data-results">
-          {filteredProducts.map((product, index) => {
-            return (
-              <a
-                className="search-item" 
-                key={index}>
-                <p>{product.nome}</p>
-              </a>
-            );
-          })}
-        </div>
-      )} */}
    </div>
   )
 }
